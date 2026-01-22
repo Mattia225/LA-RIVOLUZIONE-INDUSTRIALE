d451d751ddaf8692b4aca64c59d81ac42f4592ed
matti225 <mattiavolpato5@gmail.com>
2026-01-22 09:08:00 +0100

    Delete video.js

--- video.js
+++
@@ -1,6 +0,0 @@
-const thumb = document.querySelector('.video-thumb');
-const video = thumb.nextElementSibling;
-
-thumb.addEventListener('click', () => {
-    thumb.style.display = 'none';
-    video.play();
