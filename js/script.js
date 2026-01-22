8f04e120a0d2e8ea4f72bd547a28d24912f760ce
matti225 <mattiavolpato5@gmail.com>
2026-01-22 12:20:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const thumb = document.querySelector('.video-thumb');
 const video = thumb.nextElementSibling;
 
-
-
+// 1️⃣ Click sulla copertina → parte il video e copertina sparisce
+thumb.addEventListener('click', () => {
+    thumb.style.display = 'none';
+    video.play();
+});
 
 // 2️⃣ Se il video parte per qualsiasi motivo (play manuale o tasto controls) → copertina sparisce
 video.addEventListener('play', () => {
